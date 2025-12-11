3e4c7f72b009dedd278d7f0806ebc7df07971758
Simon <162421836+Simonlee789@users.noreply.github.com>
2025-12-11 12:56:06 +0700

    Update main.py to match bubble_sort.py

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ def main():
     raw = input("ป้อนตัวเลขที่ต้องการเรียงลำดับ (คั่นด้วยช่องว่าง): ")
     arr =list(map(int, raw.split()))
 
-    choice = input("เลือกวิธีการเรียงลำดับ (QuickSort/ Bubble): ").lower()
+    i = input("เลือกวิธีการเรียงลำดับ QuickSort (1)/ Bubble(2): ")
+    choice = int(i)
 
-    if choice == "quicksort":
+    if choice == 1:
         quick_sort(arr, 0, len(arr) - 1)
         print("ผลลัพธ์การเรียงลำดับด้วย QuickSort:", arr)
-    elif choice == "bubble":
-        result = bubble_sort(arr)
-        print("ผลลัพธ์การเรียงลำดับด้วย Bubble Sort:", result)
+    elif choice == 2:
+        bubble_sort(arr)
+        print("ผลลัพธ์การเรียงลำดับด้วย Bubble Sort:", arr)
     else:
         print("วิธีการเรียงลำดับที่เลือกไม่ถูกต้อง")
 
